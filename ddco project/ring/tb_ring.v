8906718f8f595666e36f2f6e2993736fb5cbcf74
achyutjagini <57720202+achyutjagini@users.noreply.github.com>
2020-12-10 18:44:52 +0530

    Add files via upload

---
+++ ddco project/ring/tb_ring.v
@@ -0,0 +1,33 @@
+module tb_ring;
+
+    // Inputs
+    reg Clock;
+    reg Reset;
+
+    // Outputs
+    wire [3:0] Count_out;
+
+    // Instantiate the Unit Under Test (UUT)
+    ring_counter uut (
+        .Clock(Clock), 
+        .Reset(Reset), 
+        .Count_out(Count_out)
+    );
+
+    ///////////////////Clock generation ///////////////////////////
+    initial Clock = 0;  
+
+    
+    //Simulation inputs.
+    initial begin $dumpfile("dump.vcd");
+      $dumpvars(0,Count_out); 
+
+    //Apply Reset for 50 ns.
+        Reset = 1; //Reset is high
+        
+        #50;       //Wait for 50 ns
+        Reset = 0; //Reset is low.
+    
+end
+      
+endmodule
